09d08b067385f61d41e8fe2610b7390d784ea467
Lais Correa <laiscorreamartins@gmail.com>
2022-05-10 20:23:57 -0300

    initial commit

---
+++ cadastro_produtos.js
@@ -0,0 +1,90 @@
+//const Client = require('pg').Client;
+const {Client} = require('pg');
+
+const conexao = {
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '080194', 
+    database: 'crud_produtos'
+};
+
+function inserir(produto){
+}
+
+
+
+
+function listar(callback) {
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = "SELECT * FROM produtos";
+    cliente.query(sql, 
+        function (erro, res) {
+            if(erro) {
+                callback(erro.message, undefined);
+            }
+            else {
+                let produtos = res.rows;
+                callback(undefined, produtos);     
+            }
+            cliente.end();
+        }
+    )    
+}
+
+function buscarPorId(id, callback){
+    /*
+
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = 'SELECT * FROM produtos WHERE id = ' + id;
+    cliente.query(sql, 
+        function (erro, res) {
+            if(erro) {
+                callback(erro.message, undefined);
+            }
+            else {
+                let produtos = res.rows[id];
+                callback(undefined, produtos);     
+            }
+            cliente.end();
+        }
+    )   
+    */
+
+    const cliente = new Client(conexao);
+    cliente.connect();
+    
+    const sql = 'SELECT * FROM produtos WHERE id = $1';
+    const values = [id]
+    cliente.query(sql, values, 
+        function (err, res) {
+            if(err) {
+                callback(err.message, undefined);
+            }
+            else if (res.rows && res.rows.length > 0) {
+                let produtos = res.rows;
+                callback(undefined, produtos);     
+            } else {
+                console.log('Produto não encontrado.')
+            }
+            cliente.end();
+        }
+    )   
+   
+    
+}
+
+
+function atualizar(id,produtoAlterado) {
+}
+
+function deletar(id) {
+}
+
+module.exports = {
+    inserir, listar, buscarPorId, atualizar, deletar
+}
---
+++ main.js
@@ -0,0 +1,47 @@
+const cadastroProdutos = require('./cadastro_produtos.js')
+
+//Main
+//cadastroProdutos.inserir({nome: "mesa", preco:450});
+//cadastroProdutos.inserir({nome: "cadeira", preco:180});
+//cadastroProdutos.inserir({nome: "roupeiro", preco:840});
+//1
+console.log("Listar: ");
+//2
+cadastroProdutos.listar(
+    //4
+    function(err, produtos) {
+        if(err) {
+            console.log("Sistema esta com problemas");
+            console.log(err);
+        }
+        else {
+            console.log(produtos);
+        }
+    }
+);
+//3
+console.log("Fim do Listar");
+
+console.log("BuscarPorId(2): ");
+cadastroProdutos.buscarPorId(2, function(erro, produto){
+    if (erro){
+        console.log('Erro ' + erro);
+
+    } else {
+        console.log(produto)
+    }
+});
+
+//console.log("BuscarPorId(4): ");
+//console.log(cadastroProdutos.buscarPorId(4));
+
+//console.log("Atualizar: ");
+//console.log(cadastroProdutos.atualizar(
+//    3, {id:3, nome: "roupeiro", preco:850}
+//));
+
+//console.log("Deletar: ");
+//console.log(cadastroProdutos.deletar(1));
+
+//console.log("Listar: ");
+//console.log(cadastroProdutos.listar());
